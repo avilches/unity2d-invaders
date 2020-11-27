c3afb348bcc6a459f40b8055c69e41815a2e15bc
Alberto Vilches <vilches@gmail.com>
2020-11-27 12:19:43 +0100

    Refresh

---
+++ Assets/ClickHandler.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class ClickHandler : MonoBehaviour {
+    public GameObject floatingText;
+
+	void Update () {
+	    if(floatingText != null && Input.GetMouseButtonDown(0))
+        {
+            //get mouse position
+            Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            pos.z = 0f;
+
+            //instantiate game object at point
+            Instantiate(floatingText, pos, Quaternion.identity);
+        }
+	}
+}
---
+++ Assets/FloatingText.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(TextMesh))]
+public class FloatingText : MonoBehaviour
+{
+    private Vector3 start;         //the starting point for lerping movement
+    private Vector3 end;           //ending point
+
+    public Vector3 offset = new Vector3(0f, 2f, 0f); //added relative distance to move
+    public float randomX = 0.5f;   //randomize movement left/right +/- this value
+    public float time = 2.0f;      //time to move. this with distance will determine speed
+
+    public AnimationCurve acMove;  //animation curve to move transform
+    public AnimationCurve acAlpha; //animation curve to move alpha
+
+    void Start()
+    {
+        //get start position
+        start = transform.position;
+
+        //get offset
+        end = start + offset;
+
+        //move the x randomly left/right a little
+        end.x += Random.Range(-randomX, randomX);
+
+        //start the animation
+        StartCoroutine(Animate(start, end));
+    }
+
+    IEnumerator Animate(Vector3 pos1, Vector3 pos2)
+    {
+        //current time
+        float timer = 0.0f;
+
+        //distance along animation curve, should be from 0 to 1
+        float evalMove = 0.0f;
+        float evalAlpha = 0.0f;
+
+        //get the textmesh
+        TextMesh textMesh = GetComponent<TextMesh>();
+
+        //while we still have animation time
+        while (timer <= time)
+        {
+            //get our distance for move/alpha from animation curve
+            evalMove = acMove.Evaluate(timer / time);
+            evalAlpha = 1f - acAlpha.Evaluate(timer / time);
+
+            //lerp our position
+            transform.position = Vector3.Lerp(pos1, pos2, evalMove);
+
+            //get the current color
+            Color color = textMesh.color;
+            //set the alpha and color
+            color.a = evalAlpha;
+            textMesh.color = color;
+
+            //add time passed so far
+            timer += Time.deltaTime;
+            //wait till next frame
+            yield return null;
+        }
+
+        //animation done, remove game object
+        Destroy(gameObject);
+    }
+}
